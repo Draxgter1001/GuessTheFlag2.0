eaa35cb8181da606855aabff24b910a573a4e8a0
Tafshi Uthshow Hoque <uthshow@gmail.com>
2024-02-16 13:51:55 +0000

    The Guess The Country Activity is completed. Only need to design it a little bit, but the whole functionality is complete

--- app/src/main/java/com/example/guesstheflag20/GuessTheCountryActivity.kt
+++ app/src/main/java/com/example/guesstheflag20/GuessTheCountryActivity.kt
@@ -7,11 +7,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,27 +32,87 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import com.example.guesstheflag20.ui.theme.GuessTheFlag20Theme
 import org.json.JSONObject
 import kotlin.random.Random
-
 
 class GuessTheCountryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val countriesJson = loadCountriesJson()
         val countriesList = countriesJson.keys().asSequence().map { key ->
-            key to countriesJson.getString(key) }.toList()
-
+            key to countriesJson.getString(key)
+        }.toList()
 
         setContent {
-            var currentCountryCode by remember { mutableStateOf(pickRandomCountryCode(countriesJson)) }
+            var currentCountryCode by remember {
+                mutableStateOf(pickRandomCountryCode(countriesJson))
+            }
+            var userGuess by remember { mutableStateOf("") }
+            var guessResult by remember{ mutableStateOf<Pair<Boolean, String>?>(null) }
+            var showList by remember{ mutableStateOf(false) }
+
             GuessTheFlag20Theme {
                 Column(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)
-                ){
+                    .fillMaxSize())
+                {
                     FlagImage(countryCode = currentCountryCode)
+                    TextField(
+                        value = userGuess,
+                        onValueChange = { userGuess = it },
+                        label = { Text("Select Country", style = TextStyle(fontSize =
+                        MaterialTheme.typography.bodyLarge.fontSize))},
+                        readOnly = true,
+                        modifier = Modifier.fillMaxWidth(),
+                        trailingIcon = {
+                            IconButton(onClick = { showList = !showList }) {
+                                Icon(Icons.Filled.ArrowDropDown, "dropdown")
+                            }
+                        }
+                    )
+                    if (showList) {
+                        LazyColumn(modifier = Modifier.fillMaxHeight(.5f)) {
+                            items(countriesList) { country ->
+                                TextButton(onClick = {
+                                    userGuess = country.second
+                                    showList = false
+                                }) {
+                                    Text(text = country.second, style = TextStyle(fontSize =
+                                    MaterialTheme.typography.bodyLarge.fontSize))
+                                }
+                            }
+                        }
+                    }
+                    Button(onClick = {
+                        val correctAnswer = countriesJson.getString(currentCountryCode)
+                        guessResult = Pair(userGuess == correctAnswer, correctAnswer)
+                    }, modifier = Modifier.padding(20.dp).fillMaxWidth()) {
+                        Text("Submit", style = TextStyle(fontSize =
+                        MaterialTheme.typography.bodyLarge.fontSize))
+                    }
+
+                    guessResult?.let {
+                        Text(text = if (it.first) " CORRECT!" else "Wrong",
+                            color = if (it.first) Color.Green else Color.Red,
+                            style = TextStyle(fontSize = MaterialTheme.typography.bodyLarge.fontSize),
+                            modifier = Modifier.padding(10.dp))
+                        Text(text = "The correct country was: ${it.second}",style =
+                        TextStyle(fontSize = MaterialTheme.typography.bodyLarge.fontSize),
+                            color = Color.Blue,
+                            modifier = Modifier.padding(10.dp).fillMaxWidth()
+                        )
+
+                        Button(onClick = {
+                            currentCountryCode = pickRandomCountryCode(countriesJson)
+                            userGuess = "" // Reset user guess
+                            guessResult = null // Reset for the next guess
+                        }, modifier = Modifier.padding(20.dp).fillMaxWidth()) {
+                            Text("Next", style = TextStyle(fontSize =
+                            MaterialTheme.typography.bodyLarge.fontSize))
+                        }
+                    }
                 }
             }
 
@@ -63,18 +134,14 @@ class GuessTheCountryActivity : ComponentActivity() {
     fun FlagImage(countryCode: String) {
         val context = LocalContext.current
         val resourceId = context.resources.getIdentifier(
-            countryCode.lowercase(),
-            "drawable",
-            context.packageName
+            countryCode.lowercase(), "drawable", context.packageName
         )
 
         if (resourceId != 0) {
             Image(
                 painter = painterResource(id = resourceId),
                 contentDescription = "Flag of $countryCode",
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(50.dp)
+                modifier = Modifier.fillMaxWidth().height(350.dp).padding(20.dp)
             )
         } else {
 
--- app/src/main/java/com/example/guesstheflag20/ui/theme/Color.kt
+++ app/src/main/java/com/example/guesstheflag20/ui/theme/Color.kt
@@ -10,4 +10,8 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val Red = Color(0xFFFF0000)
+val Green = Color(0xFF00FF00)
+val Blue = Color(0xFF0000FF)
+
 val Background = Color(0xFFADD8E6)
