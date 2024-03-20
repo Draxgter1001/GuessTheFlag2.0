d0599607f35a285e1f5ccae81c8721bb7118cac9
Tafshi Uthshow Hoque <uthshow@gmail.com>
2024-03-20 17:23:52 +0000

    Added some logic for the switch button

--- app/src/main/java/com/example/guesstheflag20/Buttons.kt
+++ app/src/main/java/com/example/guesstheflag20/Buttons.kt
@@ -1,9 +1,7 @@
 package com.example.guesstheflag20
 
-
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
